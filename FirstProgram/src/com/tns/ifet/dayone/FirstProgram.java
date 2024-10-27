373028ce851c64f11b90fc6100b59060639d38d3
guhan <guhan@LAPTOP-TM42P1DI>
2024-10-27 20:57:16 +0530

    Initial commit

---
+++ FirstProgram/src/com/tns/ifet/dayone/FirstProgram.java
@@ -0,0 +1,12 @@
+package com.tns.ifet.dayone;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hi welcome");
+System.out.println("I'm Guhan");
+System.out.println("From Kurinjipadi");
+	}
+
+}
---
+++ FirstProgram/src/com/tns/ifet/dayone/IdentifersDemo.java
@@ -0,0 +1,11 @@
+package com.tns.ifet.dayone;
+
+public class IdentifersDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=20;
+System.out.println(a-24-10+2-100);
+	}
+
+}
